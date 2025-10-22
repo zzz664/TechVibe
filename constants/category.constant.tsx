74cb40123b1a283498c9030eedb6b42e89fc4f15
zzz664 <zzz664>
2025-10-22 21:59:46 +0900

    chore(Sidebar): 게임 카테고리의 아이콘 변경

--- constants/category.constant.tsx
+++ constants/category.constant.tsx
@@ -1,7 +1,7 @@
-import { CodeXml, BadgeDollarSign, Puzzle } from "lucide-react";
+import { CodeXml, BadgeDollarSign, Gamepad2 } from "lucide-react";
 
 export const INSIGHT_CATEGORY = [
   { id: 1, label: "IT/프로그래밍", category:"programming", icon: <CodeXml/> },
   { id: 2, label: "암호화폐", category:"crypto", icon: <BadgeDollarSign/> },
-  { id: 3, label: "게임", category:"game", icon: <Puzzle/> },
+  { id: 3, label: "게임", category:"game", icon: <Gamepad2/> },
 ];
