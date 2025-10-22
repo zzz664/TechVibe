f5630f43afe6fcadab4665621b1d35720e02424b
zzz664 <zzz664>
2025-10-22 17:06:28 +0900

    chore(Header): Nav버튼,로고 및 로그인버튼 스타일 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -6,14 +6,14 @@ function Header() {
     <header className="fixed top-0 z-10 w-full flex items-center justify-center bg-[#121212]">
       <div className="w-full max-w-[1328px] flex items-center justify-between px-6 py-3">
         <div className="flex items-center gap-5">
-          <Image src="/logo.png" width={181} height={40} alt="@LOGO"/>
+          <Image src="/logo.png" width={181} height={40} alt="@LOGO" className="cursor-pointer"/>
           <div className="flex items-center gap-3">
-            <div className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500 cursor-pointer">Subject Insight</div>
+            <div className="prevent-drag font-semibold h-10 rounded-xl flex items-center justify-center px-3 text-white/90 hover:bg-muted-foreground/20 transition-all duration-500 cursor-pointer">Subject Insight</div>
             <Separator orientation="vertical" className="h-4! bg-muted-foreground"/>
-            <div className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500 cursor-pointer">Portfolios</div>
+            <div className="prevent-drag font-semibold h-10 rounded-xl flex items-center justify-center px-3 text-white/90 hover:bg-muted-foreground/20 transition-all duration-500 cursor-pointer">Portfolios</div>
           </div>
         </div>
-        <div className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500 cursor-pointer">Login</div>
+        <div className="prevent-drag font-semibold h-10 rounded-xl flex items-center justify-center px-3 text-white/90 hover:bg-muted-foreground/20 transition-all duration-500 cursor-pointer">Login</div>
       </div>
     </header>
   );
