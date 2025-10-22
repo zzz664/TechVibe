5c696087fb0e56a487da7cd1ac8323f5e902b8a7
zzz664 <zzz664>
2025-10-22 20:03:40 +0900

    chore(Sidebar): 카테고리 캡션을 한글로 바꿈

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -6,7 +6,7 @@ function Sidebar() {
   return (
     <aside className="min-w-60 w-60 flex flex-col gap-6">
       <div className="flex items-center gap-2">
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Category</h4>
+        <h4 className="scroll-m-20 text-xl font-extrabold tracking-tight">주제 카테고리</h4>
         <ChevronDown className="mt-1" />
       </div>
       <div className="w-full flex flex-col gap-2">
