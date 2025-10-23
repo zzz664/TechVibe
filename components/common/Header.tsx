f2982beee16d7c7b4a85290dd7f7b31a3c027bd8
zzz664 <zzz664>
2025-10-23 20:38:56 +0900

    chore(Header): 프로젝트 성격 변화에 따른 헤더 내용 수정 커뮤니티 -> 개인 블로그로 방향성을 수정하여 주제 모아보기 대신 전체 게시글로 수정함 또한 로고 클릭시 루트로 이동하도록 Link컴포넌트로 감싸줌

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import { Separator, Button } from "../ui";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="fixed top-0 z-10 w-full flex items-center justify-center bg-[#121212]">
       <div className="w-full max-w-[1328px] flex items-center justify-between px-6 py-3">
         <div className="flex items-center gap-5">
-          <Image src="/logo.png" width={181} height={40} alt="@LOGO" className="cursor-pointer"/>
+          <Link href={"/"}>
+            <Image src="/logo.png" width={181} height={40} alt="@LOGO" className="cursor-pointer"/>
+          </Link>
           <div className="flex items-center gap-3">
-            <Button variant={"ghost"} className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500">주제 모아보기</Button>
+            <Button variant={"ghost"} className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500">전체 게시글</Button>
             <Separator orientation="vertical" className="h-4! bg-muted-foreground"/>
             <Button variant={"ghost"} className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500">포트폴리오</Button>
           </div>
