de0539dd8a5a53c9616eebdbf53cdb7abf274ad4
zzz664 <zzz664>
2025-10-24 21:48:07 +0900

    feat(app/join): 중복된 닉네임 검증 로직 추가 supabase의 admin권한을 이용해서 user테이블에 닉네임 등록과정 중 UNIQUE 제약을 위반할 경우 생성했던 auth.users를 삭제하는 작업 후 실패 토스트를 띄움 supabase/server.ts - admin권한을 가진 client 생성함수 추가

--- app/join/action.ts
+++ app/join/action.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { createClient } from "@/lib/supabase/server";
+import { createAdminClient, createClient } from "@/lib/supabase/server";
 import { z } from "zod";
 import { formSchema } from "./validation";
 
@@ -11,17 +11,33 @@ export interface JoinResult {
 }
 
 export async function onSubmit(formData: z.infer<typeof formSchema>): Promise<JoinResult> {
-  const supabase = await createClient(); 
+  const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.signUp({
+  const { data:{user, session}, error } = await supabase.auth.signUp({
     email: formData.email,
     password: formData.password,
   });
 
-  if(error) {
-    return { isSuccess: false, error: "이미 회원가입이 되어있는 이메일입니다."}
+  if (error) {
+    return { isSuccess: false, error: "이미 회원가입이 되어있는 이메일입니다. 회원가입에 실패했습니다." };
   }
 
-  const userId = data?.user?.id ?? undefined;
+  if (user && session) {
+    const error = await supabase
+      .from('user')
+      .insert([
+        { id: user.id, nickname: formData.nickname },
+      ])
+      .select();
+
+    if (error) {
+      const supabase_admin = await createAdminClient();
+      await supabase_admin.auth.admin.deleteUser(user.id);
+
+      return { isSuccess: false, error: "이미 존재하는 닉네임 입니다. 회원가입에 실패했습니다." };
+    }
+  }
+
+  const userId = user?.id ?? undefined;
   return { isSuccess: true, userId };
 }
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,9 +2,11 @@
 
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createClient as createServiceClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const createClient = async () => {
   const cookieStore = await cookies();
@@ -29,3 +31,10 @@ export const createClient = async () => {
     },
   );
 };
+
+export const createAdminClient = async () => {
+  if (!supabaseUrl || !supabaseServiceRoleKey) {
+    throw new Error("SUPABASE_SERVICE_ROLE_KEY or SUPABASE_URL is not set on the server.");
+  }
+  return createServiceClient(supabaseUrl, supabaseServiceRoleKey);
+}
