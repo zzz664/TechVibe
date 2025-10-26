aed4da73694fdb6a2c5e70520c0e583c8d65fa07
zzz664 <zzz664>
2025-10-27 02:50:41 +0900

    feat(app/page): 글쓰기 버튼 로직 추가 로그인 된 세션이 있을 때 글쓰기 버튼을 클릭하면 제목,내용,카테고리,썸네일,author는 auth.user.id인 임시저장된 게시글을 1개 생성하여 insert를 수행하고 기존 /create로 redirect하는 부분을 /create/id 형식으로 redirect하도록 백틱을 사용하여 insert후 체이닝된 select data에서 추출하여 작성함 그리고 create 앱라우터는 하위에 다이나믹라우팅 [id]폴더를 추가하고 page.tsx파일을 옮겼음

--- app/action.ts
+++ app/action.ts
@@ -7,8 +7,14 @@ export const onClickNewPost = async () => {
   const supabase = await createClient();
 
   const res = await supabase.auth.getUser();
-  if(res.data.user) {
-    redirect("/create");
+  if (res.data.user) {
+    const { data, error } = await supabase
+      .from('admin_post')
+      .insert([
+        { title: null, content: null, main_category: null, sub_category: null, thumbnail: null, author: res.data.user.id},
+      ])
+      .select();
+    redirect(`/create/${data?.[0]?.id}`);
   } else {
     return { status: "failed" };
   }
