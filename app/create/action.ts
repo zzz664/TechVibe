5c7d86412aaab79885d208361b2f32b478f06ff5
zzz664 <zzz664>
2025-10-28 02:13:58 +0900

    feat(app/create): 썸네일 파일을 supabase storage에 업로드 후 public url을 얻는 로직 추가 기존 Input으로 받은 File타입에서 파일 확장자를 추출 -> 파일 이름은 nanoid 라이브러리를 이용함 supabase storage인 tech-vibe files의 posts에 nanoid().확장자 형태로 업로드 함 업로드 후 업로드한 썸네일의 public URL을 얻어옴 -> 임시저장 로직 수행

--- app/create/action.ts
+++ app/create/action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { nanoid } from "nanoid";
 import { createClient } from "@/lib/supabase/server";
 import { PostData } from "@/model";
 
@@ -11,21 +12,35 @@ export async function onClickSaveDraft(post_data: PostData) {
     if (!post_data.title && !post_data.content && !post_data.sub_category && !post_data.thumbnail) {
       return { status: "save_draft_failed" };
     } else {
-      const { data, error } = await supabase
+      //supabase storage에 파일을 업로드 후 public url을 얻는 과정
+      let thumbnailURL: string|null = null;
+
+      if (post_data.thumbnail && post_data.thumbnail instanceof File) {
+        const fileExt = post_data.thumbnail.name.split(".").pop();
+        const fileName = `${nanoid()}.${fileExt}`;
+        const filePath = `posts/${fileName}`;
+
+        const { error: upload_error } = await supabase.storage.from('tech-vibe files')
+          .upload(filePath, post_data.thumbnail);
+        const { data } = await supabase.storage.from('tech-vibe files').getPublicUrl(filePath);
+        thumbnailURL = data.publicUrl;
+      }
+
+      //썸네일의 public url을 얻고난 뒤 임시저장 로직 수행
+      const { error: save_draft_error } = await supabase
         .from('admin_post')
         .update([
           {
             title: post_data.title,
-            content: post_data.content,
+            content: JSON.stringify(post_data.content),
             main_category: post_data.main_category,
             sub_category: post_data.sub_category,
-            thumbnail: post_data.thumbnail,
+            thumbnail: thumbnailURL,
             author: res.data.user.id,
             status: "temp"
           },
         ])
-        .eq("id", +post_data.id)
-        .select();
+        .eq("id", +post_data.id);
       return { status: "save_draft_success" }
     }
   } else {
