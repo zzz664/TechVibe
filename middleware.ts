9a34cc2803d962c1dc3aff8b1f9467b81a63bcd0
zzz664 <zzz664>
2025-10-30 01:09:50 +0900

    feat(middleware): 사용자가 임시저장, 게시하기를 하지 않은 글들을 정리하는 로직 추가 beforeunload이벤트핸들러와 라우트가 변경되는 것을 감지하여 status값이 null인 임시글을 정리하는 로직을 시도했으나 beforeunload이벤트핸들러에서 새로고침, 뒤로가기, 브라우저 종료를 구분할 방법을 전혀 모색하지 못했음. 따라서 사용자는 사이트를 사용하려면 무조건 '/' 경로를 거쳐야하므로 middleware.ts에서 이를 처리하는 방법으로 해결함 middleware에서는 '/'일때만 동작하도록 config를 추가하였고 pathname을 통해 한번더 '/'인지 필터링을 진행함 이후 유저가 로그인상태인지 확인 -> 로그인 상태라면 이 유저 아이디와 일치하며 status가 null인 게시글 목록을 select함 이 select된 데이터의 길이가 0보다 크면 임시데이터가 있다는 뜻이므로 해당 데이터들의 delete를 진행한다.

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { createClient } from "./lib/supabase/server";
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/') {
+    const supabase = await createClient();
+    const { data: { user }, error: auth_error} = await supabase.auth.getUser();
+
+    if (user) {
+      const { data: temp_post, error: select_error } = await supabase.from('admin_post').select('*').eq('id', user.id).eq('status', null);
+      if(temp_post?.length as number > 0) {
+        const delete_error = await supabase.from('admin_post').delete().eq('id', user.id).eq('status', null);
+      }
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/',
+};
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["geqbqjzkhqdmbsrtfebt.supabase.co"],
+    remotePatterns: [{protocol: "https", hostname: "geqbqjzkhqdmbsrtfebt.supabase.co"}],
   },
 };
 
