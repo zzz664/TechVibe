20d80d594ffcacf8945806c7774a7146aa8bfa7f
zzz664 <zzz664>
2025-10-31 05:20:39 +0900

    delete(PostCreateComponent.tsx): 쓰지 않는 컴포넌트 파일 제거

--- components/custom/index.ts
+++ components/custom/index.ts
@@ -1,6 +1,5 @@
 export { NewPostButton } from "./NewPostButton";
 export { DraftListButton } from "./DraftListButton";
-export { PostCreateComponent } from "./PostCreateComponent";
 export { PostAreaContainer } from "./CreatePost/PostAreaContainer";
 export { PostSettingContainer } from "./CreatePost/PostSettingContainer";
 export { CategorySelector } from "./CreatePost/CategorySelector";
