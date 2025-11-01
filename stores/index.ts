3451fc4ccdc6b7a9ce0203a18241c3c46696f480
zzz664 <zzz664>
2025-11-01 21:06:05 +0900

    refactor(stores/index.ts): 기존 usePostStore를 persist를 사용하도록 수정 페이지 새로고침 시 서버에 저장된 데이터와 새로고침으로 사라져버린 zustand의 전역 상태가 동기화가 안되는 문제 발생 확실히 동기화 하고자 localStorage를 이용하도록 persist를 추가하여 'post-storage'키에 보관하도록 함

--- stores/index.ts
+++ stores/index.ts
@@ -42,27 +42,32 @@ export const useAuthStore = create<AuthStore>()(
   ),
 )
 
-export const usePostStore = create<PostStore>((set) => ({
-  postData: {
-    title: null,
-    content: null,
-    main_category: null,
-    sub_category: null,
-    thumbnail: null
-  },
-  setTitle: (new_title: string) => {
-    set((prev) => ({ postData: { ...prev.postData, title: new_title } }));
-  },
-  setContent: (new_content: Block[]) => {
-    set((prev) => ({ postData: { ...prev.postData, content: new_content } }));
-  },
-  setMainCategory: (new_main_category: string) => {
-    set((prev) => ({ postData: { ...prev.postData, main_category: new_main_category } }));
-  },
-  setSubCategory: (new_sub_category: string) => {
-    set((prev) => ({ postData: { ...prev.postData, sub_category: new_sub_category } }));
-  },
-  setThumbnail: (new_thumbnail: File | string | null) => {
-    set((prev) => ({ postData: { ...prev.postData, thumbnail: new_thumbnail } }));
-  },
-}));
+export const usePostStore = create<PostStore>()(
+  persist(
+    (set) => ({
+      postData: {
+        title: null,
+        content: null,
+        main_category: null,
+        sub_category: null,
+        thumbnail: null
+      },
+      setTitle: (new_title: string) => {
+        set((prev) => ({ postData: { ...prev.postData, title: new_title } }));
+      },
+      setContent: (new_content: Block[]) => {
+        set((prev) => ({ postData: { ...prev.postData, content: new_content } }));
+      },
+      setMainCategory: (new_main_category: string) => {
+        set((prev) => ({ postData: { ...prev.postData, main_category: new_main_category } }));
+      },
+      setSubCategory: (new_sub_category: string) => {
+        set((prev) => ({ postData: { ...prev.postData, sub_category: new_sub_category } }));
+      },
+      setThumbnail: (new_thumbnail: File | string | null) => {
+        set((prev) => ({ postData: { ...prev.postData, thumbnail: new_thumbnail } }));
+      },
+    }), 
+    { name: "post-storage", }
+  ),
+);
