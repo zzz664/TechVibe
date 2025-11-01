8b8782851f63a57344e3b1b18b80146726798a7c
zzz664 <zzz664>
2025-11-01 19:08:53 +0900

    fix(Header): LCP문제를 해결하기위해 이미지태그에 priority속성을 추가함

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -15,7 +15,7 @@ async function Header() {
       <div className="w-full max-w-[1328px] flex items-center justify-between px-6 py-3">
         <div className="flex items-center gap-5">
           <Link href={"/"}>
-            <Image src="/logo.png" width={181} height={40} alt="@LOGO" className="cursor-pointer" />
+            <Image src="/logo.png" priority width={181} height={40} alt="@LOGO" className="cursor-pointer" />
           </Link>
           <div className="flex items-center gap-3">
             <Button variant={"ghost"} className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500">전체 게시글</Button>
