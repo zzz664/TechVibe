ba84ae1bfaec62a024cea82ab8037018298d5534
zzz664 <zzz664>
2025-11-02 18:35:42 +0900

    chore(components/custom/ceate_post): 폴더경로를 CreatePost에서 create_post로 수정

--- app/create/[id]/page.tsx
+++ app/create/[id]/page.tsx
@@ -1,22 +1,32 @@
-import { CategorySelector, PostAreaContainer, PostSettingContainer, ThumnailSelector } from "@/components/custom";
+import {
+  CategorySelector,
+  PostAreaContainer,
+  PostSettingContainer,
+  ThumnailSelector,
+} from "@/components/custom";
 import { fetchPostById, onClickPublishPost, onClickSaveDraft } from "../action";
 import { SaveButtonComponent } from "@/components/custom/SaveButtonComponent";
-import { TitleInput } from "@/components/custom/CreatePost/TitleInput";
+import { TitleInput } from "@/components/custom/create_post/TitleInput";
 import { Editor } from "@/components/common";
-import { PostCreateContainer } from "@/components/custom/CreatePost/PostCreateContainer";
+import { PostCreateContainer } from "@/components/custom/create_post/PostCreateContainer";
 
-export default async function Home({ params }: { params: { id: string }}) {
-  {/*params가 thenable(이 상태에선 undefined로 나옴)일 수 있으므로 타입을 명시하여 await를 사용*/}
-  const { id } = await params as { id: string };
+export default async function Home({ params }: { params: { id: string } }) {
+  {
+    /*params가 thenable(이 상태에선 undefined로 나옴)일 수 있으므로 타입을 명시하여 await를 사용*/
+  }
+  const { id } = (await params) as { id: string };
   const { fetch_data } = await fetchPostById(+id);
 
   return (
     <PostCreateContainer original_data={fetch_data}>
       {/*임시저장, 게시 버튼영역*/}
-      {<SaveButtonComponent
-        id={fetch_data ? fetch_data.id : id}
-        onClickSaveDraft={onClickSaveDraft}
-        onClickPublishPost={onClickPublishPost} />}
+      {
+        <SaveButtonComponent
+          id={fetch_data ? fetch_data.id : id}
+          onClickSaveDraft={onClickSaveDraft}
+          onClickPublishPost={onClickPublishPost}
+        />
+      }
       {/*게시글 작성하기 영역*/}
       <PostAreaContainer>
         <TitleInput initial_title={fetch_data?.title as string} />
@@ -26,9 +36,9 @@ export default async function Home({ params }: { params: { id: string }}) {
       <PostSettingContainer>
         <CategorySelector
           initial_main_category={fetch_data?.main_category as string}
-          initial_sub_category={fetch_data?.sub_category as string} />
-        <ThumnailSelector
-          initial_thumbnail={fetch_data?.thumbnail as string} />
+          initial_sub_category={fetch_data?.sub_category as string}
+        />
+        <ThumnailSelector initial_thumbnail={fetch_data?.thumbnail as string} />
       </PostSettingContainer>
     </PostCreateContainer>
   );
--- components/custom/index.ts
+++ components/custom/index.ts
@@ -1,6 +1,6 @@
 export { NewPostButton } from "./NewPostButton";
 export { DraftListButton } from "./DraftListButton";
-export { PostAreaContainer } from "./CreatePost/PostAreaContainer";
-export { PostSettingContainer } from "./CreatePost/PostSettingContainer";
-export { CategorySelector } from "./CreatePost/CategorySelector";
-export { ThumnailSelector } from "./CreatePost/ThumbnailSelector";
+export { PostAreaContainer } from "./create_post/PostAreaContainer";
+export { PostSettingContainer } from "./create_post/PostSettingContainer";
+export { CategorySelector } from "./create_post/CategorySelector";
+export { ThumnailSelector } from "./create_post/ThumbnailSelector";
