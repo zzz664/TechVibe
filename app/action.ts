aad916fbe5088191c674eafa469fdc1151d9ecb4
zzz664 <zzz664>
2025-11-02 21:00:56 +0900

    feat(handleRecentPostList): 발행된 게시글 목록과 닉네임 표기를 위한 닉네임 정보를 가져오는 서버액션 추가 상태가 publish인 게시글들과 등록된 유저의 닉네임 정보를 가져옴. 이후 게시글은 reverse하여 최신 게시글이 맨 앞에 오도록하여 반환

--- app/action.ts
+++ app/action.ts
@@ -9,16 +9,24 @@ export const onClickNewPost = async () => {
   const res = await supabase.auth.getUser();
   if (res.data.user) {
     const { data, error } = await supabase
-      .from('admin_post')
+      .from("admin_post")
       .insert([
-        { title: null, content: null, main_category: null, sub_category: null, thumbnail: null, author: res.data.user.id, status: null },
+        {
+          title: null,
+          content: null,
+          main_category: null,
+          sub_category: null,
+          thumbnail: null,
+          author: res.data.user.id,
+          status: null,
+        },
       ])
       .select();
-    return { status: "success", url: `/create/${data?.[0]?.id}`};
+    return { status: "success", url: `/create/${data?.[0]?.id}` };
   } else {
     return { status: "failed" };
   }
-}
+};
 
 export const handleDraftList = async () => {
   const supabase = await createClient();
@@ -26,12 +34,35 @@ export const handleDraftList = async () => {
   const res = await supabase.auth.getUser();
   if (res.data.user) {
     const { data: draft_data, error } = await supabase
-    .from('admin_post')
-    .select('*')
-    .eq('author', res.data.user.id)
-    .eq('status', POST_STATUS.TEMP);
+      .from("admin_post")
+      .select("*")
+      .eq("author", res.data.user.id)
+      .eq("status", POST_STATUS.TEMP);
     return { status: "success", draft_data };
   } else {
     return { status: "failed" };
   }
-}
+};
+
+export const handleRecentPostList = async () => {
+  const supabase = await createClient();
+
+  const { data: post_data, error: post_error } = await supabase
+    .from("admin_post")
+    .select("*")
+    .eq("status", POST_STATUS.PUBLISH);
+
+  const { data: user_list, error: user_list_error } = await supabase
+    .from("user")
+    .select("*");
+  if (post_error || user_list_error) {
+    return { status: "failed" };
+  } else {
+    const reverse_post_data = post_data?.reverse();
+    return {
+      status: "success",
+      post_data: reverse_post_data,
+      user_list: user_list,
+    };
+  }
+};
