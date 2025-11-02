b97048513068abcf912e95ace91d2a14acea4ea9
zzz664 <zzz664>
2025-11-03 04:57:28 +0900

    refactor(CategorySelector): props타입에서 null도 받을 수 있게 변경함 useEffect에서 마운트시에 값이 null이 아니면 초기값을 설정하도록 수정 defaultValue설정시 initial_sub_category값이 null이면 undefined를 설정하도록 수정

--- components/custom/create_post/CategorySelector.tsx
+++ components/custom/create_post/CategorySelector.tsx
@@ -1,34 +1,48 @@
 "use client";
 
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui";
 import { POST_CATEGORY } from "@/constants/category.constant";
 import { usePostStore } from "@/stores";
 import { useEffect } from "react";
 
 type Props = {
-  initial_main_category: string;
-  initial_sub_category: string;
-}
+  initial_main_category: string | null;
+  initial_sub_category: string | null;
+};
 
-function CategorySelector({ initial_main_category, initial_sub_category }: Props) {
+function CategorySelector({
+  initial_main_category,
+  initial_sub_category,
+}: Props) {
   const { setMainCategory, setSubCategory } = usePostStore();
-  
+
   useEffect(() => {
-    setMainCategory(initial_main_category);
-    setSubCategory(initial_sub_category);
+    if (initial_main_category) setMainCategory(initial_main_category);
+    if (initial_sub_category) setSubCategory(initial_sub_category);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Select defaultValue={initial_sub_category} onValueChange={(value) => {
-      setSubCategory(value);
-      POST_CATEGORY.forEach((item) => {
-        if (item.sub_category === value) {
-          setMainCategory(item.main_category);
-          return;
-        }
-      });
-    }}>
+    <Select
+      defaultValue={initial_sub_category ?? undefined}
+      onValueChange={(value) => {
+        setSubCategory(value);
+        POST_CATEGORY.forEach((item) => {
+          if (item.sub_category === value) {
+            setMainCategory(item.main_category);
+            return;
+          }
+        });
+      }}
+    >
       <SelectTrigger className="w-full">
         <SelectValue placeholder="카테고리를 선택하세요." />
       </SelectTrigger>
