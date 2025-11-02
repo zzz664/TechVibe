3264163109bafb61992d584a9c70748e2c92f893
zzz664 <zzz664>
2025-11-02 21:23:05 +0900

    feat(NewPostCard): 실제 데이터를 출력하도록 코드 추가 ResponsePostData와 string타입을 props로 받음 parseContent함수는 JSON.parse함수를 호출하여 blocknote의 문서 구조인 각 block을 순회하며 해당 block에 content의 요소를 순회한다. content의 요소에 text가 있다면 result에 내용을 더해준다. 만약 최대 글자수를 넘어가면 그 즉시 문자열을 자르고 ...을 더해서 content의 text만 파싱한 데이터를 반환한다. 각 부분에 맞는 데이터를 출력하도록 코드를 추가함

--- components/custom/home/NewPostCard.tsx
+++ components/custom/home/NewPostCard.tsx
@@ -1,7 +1,67 @@
-import { Card } from "@/components/ui";
+import { Card, Separator } from "@/components/ui";
+import { ResponsePostData } from "@/model";
+import Image from "next/image";
 
-function NewPostCard() {
-  return <Card className="w-full h-[420px]"></Card>;
+type Props = {
+  post_data: ResponsePostData;
+  nickname: string;
+};
+
+function parseContent(content: string | undefined, maxCharacter = 200) {
+  const parsed_content =
+    typeof content === "string" ? JSON.parse(content) : null;
+
+  let result = "";
+  for (const block of parsed_content) {
+    for (const content of block.content) {
+      if (content.text) {
+        result += content.text + " ";
+      }
+      if (result.length >= maxCharacter)
+        return result.slice(0, maxCharacter) + "...";
+    }
+  }
+  return result;
+}
+
+function NewPostCard({ post_data, nickname }: Props) {
+  return (
+    <Card className="w-full h-fit p-4 flex flex-col gap-4">
+      <div className="w-full flex items-start justify-center gap-2">
+        <div className="flex-1 flex flex-col items-start justify-start gap-6">
+          <h3 className="text-base font-semibold tracking-tight line-clamp-2">
+            {post_data?.title}
+          </h3>
+          <p className="text-xs text-muted-foreground line-clamp-3">
+            {parseContent(post_data?.content)}
+          </p>
+        </div>
+        <Image
+          src={post_data?.thumbnail as string}
+          alt="@THUMBNAIL"
+          width={100}
+          height={100}
+          priority
+          className="w-[100px] h-[100px] aspect-square rounded-2xl object-cover"
+        />
+      </div>
+      <Separator />
+      <div className="w-full flex items-center justify-between">
+        <div className="flex flex-col gap-2">
+          <p>{nickname}</p>
+          <div className="flex items-start justify-start gap-2">
+            <p className="text-muted-foreground">{post_data?.main_category}</p>
+            <Separator
+              orientation="vertical"
+              className="h-6! text-muted-foreground!"
+            />
+            <p className="text-muted-foreground">{post_data?.sub_category}</p>
+          </div>
+        </div>
+        <p>조회수/좋아요/댓글</p>
+      </div>
+    </Card>
+  );
 }
 
 export { NewPostCard };
