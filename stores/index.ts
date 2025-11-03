becff77bad38a9f274f2fb8c9f41656271f80763
zzz664 <zzz664>
2025-11-04 03:41:33 +0900

    refactor(useAuthStore): 인증 관련 로직 변경으로 인한 클라이언트 인증상태 관리를 위해 Zustand 전역상태 변경 기존에는 인증을 서버액션으로 처리했는데 이는 클라이언트와 서버의 인증상태 동기화 작업이 뭔가 매끄럽게 되지 않는다고 판단함 그래서 클라이언트용 supabase 인스턴스를 이용하여 인증하는 방식으로 바꿈에 따라 useAuthStore의 전역상태에는 유저의 id값과 nickname만 저장하도록 변경 로그인시 해당 값이 채워지고 로그아웃 시 null로 초기화 됨

--- stores/index.ts
+++ stores/index.ts
@@ -3,17 +3,12 @@ import { Block } from "@blocknote/core";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-interface UserInfo {
-  isAuth: boolean;
-  id: string;
-  nickname: string;
-}
-
 interface AuthStore {
-  user_info: UserInfo;
+  user_id: string | undefined | null;
+  nickname: string | null;
 
-  setUser: (newUserInfo: UserInfo) => void;
-  resetUser: () => void;
+  setUserId: (user_id: string | undefined | null) => void;
+  setNickname: (nickname: string | null) => void;
 }
 
 type PostDataWithoutId = Omit<PostData, "id">;
@@ -30,17 +25,14 @@ interface PostStore {
 export const useAuthStore = create<AuthStore>()(
   persist(
     (set) => ({
-      user_info: {
-        isAuth: false,
-        id: "",
-        nickname: "",
-      },
-      setUser: (newUserInfo: UserInfo) => set(() => ({ user_info: newUserInfo })),
-      resetUser: () => set(() => ({ user_info: { isAuth: false, id: "", nickname: "" } })),
+      user_id: null,
+      nickname: null,
+      setUserId: (user_id) => set({ user_id: user_id }),
+      setNickname: (nickname) => set({ nickname: nickname }),
     }),
-    { name: "auth-storage", }
-  ),
-)
+    { name: "auth-storage" }
+  )
+);
 
 export const usePostStore = create<PostStore>()(
   persist(
@@ -50,24 +42,32 @@ export const usePostStore = create<PostStore>()(
         content: null,
         main_category: null,
         sub_category: null,
-        thumbnail: null
+        thumbnail: null,
       },
       setTitle: (new_title: string) => {
         set((prev) => ({ postData: { ...prev.postData, title: new_title } }));
       },
       setContent: (new_content: Block[]) => {
-        set((prev) => ({ postData: { ...prev.postData, content: new_content } }));
+        set((prev) => ({
+          postData: { ...prev.postData, content: new_content },
+        }));
       },
       setMainCategory: (new_main_category: string) => {
-        set((prev) => ({ postData: { ...prev.postData, main_category: new_main_category } }));
+        set((prev) => ({
+          postData: { ...prev.postData, main_category: new_main_category },
+        }));
       },
       setSubCategory: (new_sub_category: string) => {
-        set((prev) => ({ postData: { ...prev.postData, sub_category: new_sub_category } }));
+        set((prev) => ({
+          postData: { ...prev.postData, sub_category: new_sub_category },
+        }));
       },
       setThumbnail: (new_thumbnail: File | string | null) => {
-        set((prev) => ({ postData: { ...prev.postData, thumbnail: new_thumbnail } }));
+        set((prev) => ({
+          postData: { ...prev.postData, thumbnail: new_thumbnail },
+        }));
       },
-    }), 
-    { name: "post-storage", }
-  ),
+    }),
+    { name: "post-storage" }
+  )
 );
