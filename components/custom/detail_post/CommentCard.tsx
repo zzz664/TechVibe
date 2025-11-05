97558566ee3c23dd5c4c97a2f8ce70659bed2372
zzz664 <zzz664>
2025-11-05 16:29:42 +0900

    feat(CommentCard): 실제 데이터와 연동

--- components/custom/detail_post/CommentCard.tsx
+++ components/custom/detail_post/CommentCard.tsx
@@ -1,14 +1,18 @@
 import { Card, Separator } from "@/components/ui";
+import { CommentData } from "@/model";
+import dayjs from "dayjs";
 
-function CommentCard() {
+function CommentCard({ data }: { data: CommentData }) {
   return (
     <Card className="flex flex-col items-start justify-start gap-2 px-5 py-3 border-0 bg-accent/20">
       <div className="w-full flex items-center justify-between">
-        <p>작성자: test</p>
-        <p className="text-muted-foreground">2025.11.03</p>
+        <p>작성자: {data.nickname}</p>
+        <p className="text-muted-foreground">
+          {dayjs(data.created_at).format("YYYY.MM.DD")}
+        </p>
       </div>
       <Separator />
-      <p className="h-15 text-muted-foreground">댓글내용~~~~~~~~~~~~~~~~~~</p>
+      <p className="h-15 text-muted-foreground">{data.content}</p>
     </Card>
   );
 }
