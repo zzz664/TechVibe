eae109a55fe20c854378eb35c7b20767526501ca
zzz664 <zzz664>
2025-11-05 16:31:13 +0900

    chore(app/post/action.ts): fetchPostById함수 수정 관계된 댓글 컬럼 전부를 가져오기보단 댓글의 id, content, created_at, nickname만 가져오도록 수정

--- app/post/action.ts
+++ app/post/action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
-import { POST_STATUS, ResponsePostData } from "@/model/post_model";
+import { POST_STATUS, ResponsePostDataWithComment } from "@/model/post_model";
 import { revalidatePath } from "next/cache";
 
 export async function fetchPublishPostById(id: number) {
@@ -9,11 +9,13 @@ export async function fetchPublishPostById(id: number) {
 
   const { data, error } = await supabase
     .from("admin_post")
-    .select("*")
+    .select("*, comment(id,created_at,content,nickname)")
     .eq("id", id)
-    .eq("status", POST_STATUS.PUBLISH);
+    .eq("status", POST_STATUS.PUBLISH)
+    .single();
 
-  const post_data: ResponsePostData = data?.[0] as ResponsePostData;
+  const post_data: ResponsePostDataWithComment =
+    data as ResponsePostDataWithComment;
 
   if (error) {
     return { fetch_data: null, status: "failed" };
@@ -34,7 +36,11 @@ export async function fetchUserId() {
   return { userId: data.user.id, status: "success" };
 }
 
-export async function saveComment(post_id: string, content: string) {
+export async function saveComment(
+  post_id: string,
+  content: string,
+  nickname: string | null
+) {
   if (!content) {
     return { status: "blank content" };
   }
@@ -47,11 +53,14 @@ export async function saveComment(post_id: string, content: string) {
     return { status: "auth failed" };
   }
 
-  const { error: save_error } = await supabase
-    .from("comment")
-    .insert([
-      { post_id: post_id, content: content, user_id: auth_data.user.id },
-    ]);
+  const { error: save_error } = await supabase.from("comment").insert([
+    {
+      post_id: post_id,
+      content: content,
+      user_id: auth_data.user.id,
+      nickname: nickname,
+    },
+  ]);
 
   if (save_error) {
     return { status: "save failed" };
