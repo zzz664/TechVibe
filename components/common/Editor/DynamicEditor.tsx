b9f48d17d46f0daa7128273b55af3aa25916ab45
zzz664 <zzz664>
2025-11-05 19:49:02 +0900

    feat(Editor): 로딩 UI 추가 dynamic을 이용한 동적 임포트를 사용함에 따라 사용자 경험 개선을 위해 loading ui를 추가함

--- components/common/Editor/DynamicEditor.tsx
+++ components/common/Editor/DynamicEditor.tsx
@@ -1,3 +1,16 @@
 "use client";
+import { Spinner } from "@/components/ui";
 import dynamic from "next/dynamic";
-export const Editor = dynamic(() => import("./Editor"), { ssr: false });
+export const Editor = dynamic(() => import("./Editor"), {
+  ssr: false,
+  loading: () => {
+    return (
+      <div className="flex items-center justify-center">
+        <div className="flex gap-2 text-muted-foreground">
+          <Spinner />
+          에디터를 로딩중입니다...
+        </div>
+      </div>
+    );
+  },
+});
