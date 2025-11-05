5837e559cdc591b2a60b864fb13d37e867919166
zzz664 <zzz664>
2025-11-05 15:35:56 +0900

    feat(CommentInput): 댓글 등록 로직 구현 app/post/action.ts에 saveComment함수를 작성하여 댓글을 데이터베이스에 저장하는 로직을 작성했음 빈칸 체크 -> 인증 체크 -> 저장 순서로 진행하며 저장 후 revaildatePath를 호출하여 해당 포스트의 캐시를 무효화하여 댓글이 갱신되도록 함. CommentInput에선 saveComment호출 후 응답 상태에 따라 toast메세지를 출력하도록 작성

--- app/post/action.ts
+++ app/post/action.ts
@@ -1,5 +1,8 @@
+"use server";
+
 import { createClient } from "@/lib/supabase/server";
 import { POST_STATUS, ResponsePostData } from "@/model/post_model";
+import { revalidatePath } from "next/cache";
 
 export async function fetchPublishPostById(id: number) {
   const supabase = await createClient();
@@ -29,4 +32,31 @@ export async function fetchUserId() {
   }
 
   return { userId: data.user.id, status: "success" };
+}
+
+export async function saveComment(post_id: string, content: string) {
+  if (!content) {
+    return { status: "blank content" };
+  }
+
+  const supabase = await createClient();
+
+  const { data: auth_data, error: auth_error } = await supabase.auth.getUser();
+
+  if (auth_error) {
+    return { status: "auth failed" };
+  }
+
+  const { error: save_error } = await supabase
+    .from("comment")
+    .insert([
+      { post_id: post_id, content: content, user_id: auth_data.user.id },
+    ]);
+
+  if (save_error) {
+    return { status: "save failed" };
+  }
+
+  revalidatePath(`/post/${post_id}`);
+  return { status: "success" };
 }
--- components/custom/detail_post/CommentInput.tsx
+++ components/custom/detail_post/CommentInput.tsx
@@ -1,14 +1,43 @@
-import { Button, Textarea } from "@/components/ui";
+"use client";
 
-function CommentInput() {
+import { Button, Textarea } from "@/components/ui";
+import { saveComment } from "@/app/post/action";
+import { useRef } from "react";
+import { toast } from "sonner";
+
+function CommentInput({ post_id }: { post_id: string }) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
   return (
     <div className="w-full flex flex-col justify-center gap-4">
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
         댓글 작성
       </h3>
       <div className="h-25 flex items-center justify-center gap-4 mb-10">
-        <Textarea className="h-full resize-none border-0" />
+        <Textarea className="h-full resize-none border-0" ref={textareaRef} />
         <Button
+          onClick={async () => {
+            const res = await saveComment(
+              post_id,
+              textareaRef.current?.value ?? ""
+            );
+            switch (res.status) {
+              case "blank content":
+                toast.error("댓글은 빈 칸으로 등록할 수 없습니다.");
+                break;
+              case "auth failed":
+                toast.error("댓글은 로그인을 해야 등록할 수 있습니다.");
+                break;
+              case "save failed":
+                toast.error("댓글 등록에 실패했습니다.");
+                break;
+              case "success":
+                toast.success("댓글 등록에 성공했습니다.");
+                break;
+              default:
+                break;
+            }
+          }}
           variant={"outline"}
           className="w-25 h-full bg-green-700/70! hover:bg-green-700/85! text-2xl border-0"
         >
