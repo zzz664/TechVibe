2c025ce9b53ee24fb4ae3042e77182d02e8e285c
zzz664 <zzz664>
2025-11-11 00:22:32 +0900

    refactor(app/post): fetchPublishPostByCat호출 및 PostCard 컴포넌트 추가 fetchPublishPostByCat호출 후 응답 온 데이터로 PostCard에 데이터를 전달하여 렌더링 진행 Link 컴포넌트를 사용하여 각 카드 클릭 시 이동할 수 있도록 함 게시글이 없다면 해당 카테고리에 발행 된 게시글이 없다는 걸 보여줌

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,41 @@
+import { Sidebar } from "@/components/common";
+import { PostCard } from "@/components/custom";
+import { fetchPublishPostByCat } from "./action";
+import { ResponsePostDataPlus } from "@/model";
+import Link from "next/link";
+
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ category: string }>;
 }) {
   const { category } = await searchParams;
+  const res = await fetchPublishPostByCat(category);
 
-  return <div>카테고리 : {category ? category : "전체 게시글"}</div>;
+  return (
+    <main className="w-full h-full min-h-[720px] flex flex-col gap-6 p-6">
+      <section className="w-full flex gap-4">
+        <Sidebar />
+        {res.fetch_data && res.fetch_data.length > 0 ? (
+          <div className="w-full h-85 grid grid-cols-3 gap-6">
+            {res.fetch_data.map((data: ResponsePostDataPlus) => {
+              return (
+                <Link key={data.id} href={`/post/${data.id}`}>
+                  <PostCard
+                    post_data={data}
+                    nickname={data.user.nickname}
+                    comment_count={data.comment[0].count}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="w-full min-h-140 flex items-center justify-center text-muted-foreground">
+            해당 카테고리에 발행 된 게시글이 존재하지 않습니다.
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
