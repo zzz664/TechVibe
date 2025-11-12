fe8b7e0561c60c77391a026c54d085039d38d21b
zzz664 <zzz664>
2025-11-13 06:44:11 +0900

    refactor(ThumbnailSelector): 카테고리별 기본 썸네일 선택 기능 추가 및 스타일 변경 next/image의 Image 컴포넌트가 제대로 이미지를 최적화 할 수 있도록 스타일 변경 및 sizes prop을 추가, 100em을 지정하여 부모 div의 크기에 맞도록 함 썸네일 삭제 버튼 위쪽에 카테고리 기본 썸네일 사용 버튼을 추가함 supabase storage에 미리 업로드 되어있는 카테고리_default.webp 형식의 썸네일을 지정함

--- components/custom/create_post/ThumbnailSelector.tsx
+++ components/custom/create_post/ThumbnailSelector.tsx
@@ -5,6 +5,7 @@ import { Button, Input } from "../../ui";
 import Image from "next/image";
 import { ImageUp, Trash } from "lucide-react";
 import { usePostStore } from "@/stores";
+import { DEFAULT_THUMBNAIL_URL } from "@/constants/thumbnail.constant";
 
 type Props = {
   initial_thumbnail: string | null;
@@ -21,27 +22,75 @@ function ThumnailSelector({ initial_thumbnail }: Props) {
 
   const handlePreviewThumbnail = () => {
     if (typeof postData.thumbnail === "string") {
-      return <Image src={postData.thumbnail as string} alt={"@THUMBNAIL"} priority width={100} height={100} className="w-full aspect-video rounded-lg object-cover border" />
+      return (
+        <div className="relative w-full aspect-video">
+          <Image
+            src={postData.thumbnail as string}
+            alt={"@THUMBNAIL"}
+            priority
+            sizes="100em"
+            fill
+            className="rounded-lg object-cover border"
+          />
+        </div>
+      );
     } else if (postData.thumbnail instanceof File) {
-      return <Image src={URL.createObjectURL(postData.thumbnail as File)} alt={"@THUMBNAIL"} priority width={100} height={100} className="w-full aspect-video rounded-lg object-cover border" />
+      return (
+        <div className="relative w-full aspect-video">
+          <Image
+            src={URL.createObjectURL(postData.thumbnail as File)}
+            alt={"@THUMBNAIL"}
+            priority
+            sizes="100em"
+            fill
+            className="rounded-lg object-cover border"
+          />
+        </div>
+      );
     }
     return (
       <div className="w-full flex items-center justify-center aspect-video bg-card/50 rounded-lg">
-        <Button size={"icon"} variant={"ghost"} onClick={() => fileInput.current?.click()}>
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          onClick={() => fileInput.current?.click()}
+        >
           <ImageUp />
         </Button>
       </div>
     );
-  }
+  };
 
   return (
     <div className="flex flex-col gap-2">
       {handlePreviewThumbnail()}
-      <Input type="file" accept="image/*" ref={fileInput} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        setThumbnail(e.target.files?.[0] ?? null);
-        e.target.value = "";
-      }} className="hidden" />
-      <Button variant={"outline"} className="border-0" onClick={() => setThumbnail(null)}>
+      <Input
+        type="file"
+        accept="image/*"
+        ref={fileInput}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setThumbnail(e.target.files?.[0] ?? null);
+          e.target.value = "";
+        }}
+        className="hidden"
+      />
+      <Button
+        variant={"outline"}
+        className="border-0"
+        onClick={() => {
+          const thumbnail_src =
+            DEFAULT_THUMBNAIL_URL + postData.sub_category + "_default.webp";
+          setThumbnail(thumbnail_src);
+        }}
+      >
+        <ImageUp />
+        카테고리 기본 썸네일 사용
+      </Button>
+      <Button
+        variant={"outline"}
+        className="border-0"
+        onClick={() => setThumbnail(null)}
+      >
         <Trash />
         썸네일 제거
       </Button>
