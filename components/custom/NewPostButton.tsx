736013515955f54976eaca5ceb428ccc5501c549
zzz664 <zzz664>
2025-11-12 18:40:09 +0900

    chore(NewPostButton): 스타일 변경 버튼의 variant prop제거 및 텍스트 색상을 흰색으로 배경색을 destructive의 70%로 적용 및 호버시 85%로 적용하도록 수정

--- components/custom/NewPostButton.tsx
+++ components/custom/NewPostButton.tsx
@@ -6,23 +6,25 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
 type Props = {
-  onClickNewPost: () => Promise<{ status: string, url?: string }>;
-}
+  onClickNewPost: () => Promise<{ status: string; url?: string }>;
+};
 
 function NewPostButton(props: Props) {
   const router = useRouter();
 
   return (
-    <Button variant={"destructive"} onClick={async () => {
-          const res = await props.onClickNewPost();
-          if (res?.status === "failed") {
-            toast.error("관리자 로그인이 필요한 기능입니다.");
-          } else if(res?.status === "success") {
-            router.push(res?.url as string);
-          }
-        }} className="px-5! py-5! rounded-full">
-          <PencilLine />
-          새 게시글 작성하러가기
+    <Button
+      onClick={async () => {
+        const res = await props.onClickNewPost();
+        if (res?.status === "failed") {
+          toast.error("관리자 로그인이 필요한 기능입니다.");
+        } else if (res?.status === "success") {
+          router.push(res?.url as string);
+        }
+      }}
+      className="px-5! py-5! rounded-full text-white bg-destructive/70! hover:bg-destructive/85!"
+    >
+      <PencilLine />새 게시글 작성하러가기
     </Button>
   );
 }
