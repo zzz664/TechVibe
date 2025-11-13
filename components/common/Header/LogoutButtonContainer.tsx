479b4d273cd287fc369bb8416f456dffd1c6b4e8
zzz664 <zzz664>
2025-11-13 22:11:55 +0900

    refactor(LogoutButtonContainer): 모바일용 UI 추가 764px 이상일 때는 모바일용 ui를 숨길 수 있도록 md:hidden 클래스를 지정한 요소 추가 shadcn ui의 popover 컴포넌트를 활용하여 사람 아이콘을 클릭 시 popover의 트리거가 작동하여 환영문구와 로그아웃 버튼을 볼 수 있도록 했다

--- components/common/Header/LogoutButtonContainer.tsx
+++ components/common/Header/LogoutButtonContainer.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { Button, Separator } from "@/components/ui";
+import {
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Separator,
+} from "@/components/ui";
 import { createClient } from "@/lib/supabase/client";
 import { useMemo } from "react";
+import { User } from "lucide-react";
 
 function LogoutButtonContainer({ nickname }: { nickname: unknown | null }) {
   const supabase = useMemo(() => {
@@ -15,21 +22,48 @@ function LogoutButtonContainer({ nickname }: { nickname: unknown | null }) {
   };
 
   return (
-    <div className="flex items-center gap-3">
-      <div className="mr-3 text_xs font-semibold">
-        {nickname as string}님 환영합니다.
+    <>
+      <div className="md:flex md:items-center md:gap-3 hidden">
+        <div className="mr-3 text_xs font-semibold">
+          {nickname as string}님 환영합니다.
+        </div>
+        <Separator orientation="vertical" className="h-4!" />
+        <Button
+          onClick={() => {
+            handleLogout();
+          }}
+          variant={"ghost"}
+          className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500"
+        >
+          로그아웃
+        </Button>
       </div>
-      <Separator orientation="vertical" className="h-4!" />
-      <Button
-        onClick={() => {
-          handleLogout();
-        }}
-        variant={"ghost"}
-        className="prevent-drag font-semibold text-muted-foreground hover:text-white transition-all duration-500"
-      >
-        로그아웃
-      </Button>
-    </div>
+      <div className="md:hidden">
+        <Popover>
+          <PopoverTrigger>
+            <User className="p-2 w-10 h-10 hover:bg-accent/85 cursor-pointer rounded-sm" />
+          </PopoverTrigger>
+          <PopoverContent>
+            <div className="flex flex-col gap-4">
+              <div className="text_xs font-semibold">
+                {nickname as string}님 환영합니다.
+                <p>즐겁고 보람찬 하루 보내세요!</p>
+              </div>
+              <Separator />
+              <Button
+                onClick={() => {
+                  handleLogout();
+                }}
+                variant={"outline"}
+                className="prevent-drag border-0 font-semibold text-muted-foreground hover:text-white transition-all duration-500"
+              >
+                로그아웃
+              </Button>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </>
   );
 }
 
