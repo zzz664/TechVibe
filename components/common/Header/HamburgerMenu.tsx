978023f7cc38daf240dd0ad51bed76c893961770
zzz664 <zzz664>
2025-11-14 03:14:54 +0900

    feat(HamburgerMenu): 모바일환경에서 사용될 햄버거 메뉴를 추가 햄버거 메뉴는 md분기점에서 hidden이 되고 764px이하에서 등장한다. 기존 pc환경 sidebar의 내용과 똑같으나 햄버거 버튼을 클릭하지 않았을경우 좌측화면 밖에 위치하고 햄버거 버튼이 눌리면 우측으로 이동하여 카테고리 메뉴를 보여준다. 또한 햄버거버튼 클릭 시 뒷 배경이 blur 처리 된다. blur처리된 곳을 클릭 시 햄버거 메뉴가 닫힌다. 또한 카테고리를 클릭하여 /post?category=~~~ 로 이동시에도 햄버거 메뉴는 닫힌다 hasInteracted 값을 이용하여 렌더링 초기에는 햄버거 버튼의 close 애니메이션이 나오지 않도록 함 햄버거 버튼의 3개의 가로 막대는 각각 absolute 옵션과 커스텀 애니메이션 클래스를 사용하여 클릭되어 메뉴가 열려있을때는 x모양으로 닫혀있을때는 x에서 다시 3개의 가로막대 모양으로 바뀌도록 함

---
+++ components/common/Header/HamburgerMenu.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Label, Separator } from "@/components/ui";
+import { MAIN_CATEGORYS, SUB_CATEGORYS } from "@/constants/category.constant";
+import { FileText } from "lucide-react";
+import Link from "next/link";
+import { Fragment, useState } from "react";
+
+function HamburgerMenu() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [hasInteracted, setHasInteracted] = useState<boolean>(false);
+
+  return (
+    <>
+      <div
+        onClick={() => {
+          setOpen(!open);
+          if (!hasInteracted) setHasInteracted(true);
+        }}
+        className="md:hidden relative w-10 h-10 hover:bg-accent/70 rounded-sm cursor-pointer z-50"
+      >
+        <div
+          className={`absolute bg-white w-8 h-1 top-2 left-1 border rounded-full transition-all duration-100 ${
+            open ? "animate-top-open" : hasInteracted ? "animate-top-close" : ""
+          }`}
+        />
+        <div
+          className={`absolute bg-white w-8 h-1 top-4.5 left-1 border rounded-full transition-all duration-100 ${
+            open ? "animate-mid-open" : hasInteracted ? "animate-mid-close" : ""
+          }`}
+        />
+        <div
+          className={`absolute bg-white w-8 h-1 top-7 left-1 border rounded-full transition-all duration-100 ${
+            open ? "animate-bot-open" : hasInteracted ? "animate-bot-close" : ""
+          }`}
+        />
+      </div>
+      <aside
+        className={`md:hidden fixed top-0 -left-60 w-60 h-full bg-neutral-900 z-45 transition-all ease-in-out duration-500 ${
+          open ? "translate-x-full" : "translate-x-0"
+        }`}
+      >
+        <div className="flex flex-col gap-4 p-5 pt-17">
+          {MAIN_CATEGORYS.map((main_item) => {
+            return (
+              <div key={main_item.id} className="w-full flex flex-col gap-2">
+                <div className="flex items-center gap-2">
+                  {main_item.icon}
+                  <Label className="text-base font-extrabold">
+                    {main_item.label}
+                  </Label>
+                </div>
+                <Separator />
+                {SUB_CATEGORYS.map((sub_item) => {
+                  if (sub_item.main_category === main_item.category) {
+                    return (
+                      <Fragment key={sub_item.id}>
+                        <Link
+                          href={`/post?category=${sub_item.sub_category}`}
+                          className="flex items-center gap-2 w-full h-10 text-muted-foreground hover:bg-accent hover:pl-4 hover:text-white transition-all duration-300 rounded-sm"
+                          onClick={() => {
+                            setOpen(false);
+                          }}
+                        >
+                          <FileText size={16} />
+                          <p className="font-semibold text-xs">
+                            {sub_item.label.toUpperCase()}
+                          </p>
+                        </Link>
+                        <Separator />
+                      </Fragment>
+                    );
+                  }
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </aside>
+      {open ? (
+        <div
+          className="md:hidden fixed top-0 left-0 z-40 w-full h-full bg-background/30 backdrop-blur-[2px]"
+          onClick={() => {
+            setOpen(false);
+          }}
+        />
+      ) : null}
+    </>
+  );
+}
+
+export { HamburgerMenu };
