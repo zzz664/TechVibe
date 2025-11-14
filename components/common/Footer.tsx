396931fdd00899260066d6efe0590be9c74bfbfd
zzz664 <zzz664>
2025-11-15 05:40:26 +0900

    chore(Footer): 모바일환경을 위한 스타일 추가 간단하게 글씨 크기만 작아지도록 수정했음 FIRST NEXT PROJECT 부분의 불필요한 transition효과 제거

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -6,47 +6,74 @@ function Footer() {
     <footer className="w-full flex flex-col items-center justify-center bg-[#121212]">
       <div className="w-full max-w-[1328px] flex flex-col gap-6 p-6 pb-18">
         <div className="w-full flex items-start justify-between">
-          <div className="flex flex-col items-start gap-4">
+          <div className="flex flex-col items-start gap-4 pr-2">
             <div className="flex flex-col items-start">
-              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">끝없이 발전하는 세상에서,</h3>
-              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">도태되지 않는 사람으로 살아가자.</h3>
+              <h3 className="scroll-m-20 text-base md:text-2xl font-semibold tracking-tight">
+                끝없이 발전하는 세상에서,
+              </h3>
+              <h3 className="scroll-m-20 text-base md:text-2xl font-semibold tracking-tight">
+                도태되지 않는 사람으로 살아가자.
+              </h3>
             </div>
             <div className="flex items-center gap-4">
               <Button variant={"outline"} size={"icon"} className="border-0">
-                <Image src="/kakao.png" width={240} height={240} alt="@KAKAO"/>
+                <Image src="/kakao.png" width={240} height={240} alt="@KAKAO" />
               </Button>
               <Button variant={"outline"} size={"icon"} className="border-0">
-                <Image src="/github-white.png" width={240} height={240} alt="@GITHUB"/>
+                <Image
+                  src="/github-white.png"
+                  width={240}
+                  height={240}
+                  alt="@GITHUB"
+                />
               </Button>
             </div>
           </div>
-          <div className="flex items-center gap-2">
-            <p className="prevent-drag cursor-pointer transition-all hover:font-bold duration-500">FIRST</p>
-            <Separator orientation="vertical" className="h-7!"/>
-            <p className="prevent-drag cursor-pointer transition-all hover:font-bold duration-500">NEXT.JS</p>
-            <Separator orientation="vertical" className="h-7!"/>
-            <p className="prevent-drag cursor-pointer transition-all hover:font-bold duration-500">PROJECT</p>
+          <div className="flex items-center gap-2 pl-2">
+            <p className="prevent-drag text-xs md:text-lg">FIRST</p>
+            <Separator orientation="vertical" className="h-7!" />
+            <p className="prevent-drag text-xs md:text-lg">NEXT.JS</p>
+            <Separator orientation="vertical" className="h-7!" />
+            <p className="prevent-drag text-xs md:text-lg">PROJECT</p>
           </div>
         </div>
-        <Separator/>
+        <Separator />
         <div className="w-full flex items-start justify-between">
-          <div className="h-full flex flex-col gap-2 justify-between">
-            <p className="text-[20px] font-semibold">CONTACT US</p>
+          <div className="h-full flex flex-col gap-2 justify-between pr-2">
+            <p className="text-[16px]/[1.5] md:text-xl font-semibold">
+              CONTACT US
+            </p>
             <div className="flex flex-col gap-1">
-              <p className="text-xs">평일 오전 9시 ~ 오후 6시</p>
-              <p className="text-xs">e-mail : temperis0512@gmail.com</p>
-              <p className="text-xs">또는 카카오 링크로 연락주세요.</p>
+              <p className="text-[11px]/[1.3] md:text-xs">
+                평일 오전 9시 ~ 오후 6시
+              </p>
+              <p className="text-[11px]/[1.3] md:text-xs">
+                e-mail : temperis0512@gmail.com
+              </p>
+              <p className="text-[11px]/[1.3] md:text-xs">
+                또는 카카오 링크로 연락주세요.
+              </p>
             </div>
-            <p className="text-xs">© Seonwoo-Lee all rights reserved</p>
+            <p className="text-[11px]/[1.3] md:text-xs">
+              © Seonwoo-Lee all rights reserved
+            </p>
           </div>
-          <div className="flex flex-col gap-2 mr-[10px]">
-            <p className="text-[20px] font-semibold">제작자 정보</p>
-            <div className="flex flex-col gap-2">
-              <p className="text-xs">제작자 : 이선우</p>
-              <p className="text-xs">출생 : 1998-09-19</p>
-              <p className="text-xs">선린 인터넷 고등학교 졸업</p>
-              <p className="text-xs">동국대학교 컴퓨터공학과 졸업</p>
-              <p className="text-xs">現 프론트엔드 개발자 취준생</p>
+          <div className="h-full flex flex-col gap-2 justify-between pl-2">
+            <p className="text-[16px]/[1.5] md:text-xl font-semibold">
+              제작자 정보
+            </p>
+            <div className="flex flex-col gap-1">
+              <p className="text-[11px]/[1.3] md:text-xs">제작자 : 이선우</p>
+              <p className="text-[11px]/[1.3] md:text-xs">출생 : 1998-09-19</p>
+              <p className="text-[11px]/[1.3] md:text-xs">
+                선린 인터넷 고등학교 졸업
+              </p>
+              <p className="text-[11px]/[1.3] md:text-xs">
+                동국대학교 컴퓨터공학과 졸업
+              </p>
+              <p className="text-[11px]/[1.3] md:text-xs">
+                現 프론트엔드 개발자 취준생
+              </p>
             </div>
           </div>
         </div>
