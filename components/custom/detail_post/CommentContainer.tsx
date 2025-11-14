f6136a2296076eced59733c3d1bbd243a2e6603e
zzz664 <zzz664>
2025-11-15 06:04:40 +0900

    chore(CommentContainer): 스타일 수정 모바일 환경에서 댓글 이라는 제목의 글씨 크기가 작아지도록 수정함

--- components/custom/detail_post/CommentContainer.tsx
+++ components/custom/detail_post/CommentContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 function CommentContainer({ children }: Props) {
   return (
     <div className="w-full flex flex-col justify-center gap-4">
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight">
         댓글
       </h3>
       {children}
