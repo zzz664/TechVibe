32937ebb245cf1f07d5688000c375700f4a77ad1
zzz664 <zzz664>
2025-11-20 19:28:10 +0900

    chore(app/join): 회원가입 페이지의 최소 높이를 변경

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -2,7 +2,7 @@ import { JoinForm } from "@/components/custom";
 
 export default function Home() {
   return (
-    <main className="w-full h-full min-h-[720px] flex p-6 gap-6 items-start justify-center">
+    <main className="w-full h-full min-h-[560px] flex p-6 gap-6 items-start justify-center">
       {/*회원가입 폼 영역*/}
       <div className="w-100 flex flex-col gap-6">
         <div className="flex flex-col">
