4f42caf1445ba7ac667f853c143ee4a20f823c3e
zzz664 <zzz664>
2025-11-21 21:58:56 +0900

    refactor(app/page): 데이터를 가져오는 부분 수정 기존에 await를 이용하여 비동기 처리를 순차적으로 하는 부분의 await를 제거한 후 promise를 받도록 수정 각 promise를 컴포넌트로 넘겨 처리하도록 함 promise를 전달받는 컴포넌트는 suspense를 이용하여 promise처리중엔 fallback ui를 출력하도록 함

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Flame, Newspaper } from "lucide-react";
-import { DraftDialog, Sidebar } from "../components/common";
+import { Sidebar } from "../components/common";
 import {
   fetchPopularPost,
   handleDraftList,
@@ -8,98 +8,23 @@ import {
 } from "./action";
 import {
   NewPostButton,
-  DraftListButton,
-  NewPostCard,
-  PopularPostCard,
+  PopularPostList,
+  NewPostList,
+  DraftList,
+  PostListSkeleton,
 } from "@/components/custom";
-import { ResponsePostData, ResponsePostDataPlus } from "@/model";
-import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function Home() {
-  const draft_res = await handleDraftList();
-  const recent_post_res = await handleRecentPostList();
-  const popular_post_res = await fetchPopularPost();
-
-  const renderDraftDialog = () => {
-    if (draft_res.status === "success") {
-      const existDraft =
-        (draft_res.draft_data?.length as number) > 0 ? true : false;
-      return (
-        <DraftDialog draft_data={draft_res.draft_data as ResponsePostData[]}>
-          <div className="relative">
-            <DraftListButton existDraft={existDraft} />
-          </div>
-        </DraftDialog>
-      );
-    } else if (draft_res.status === "failed") {
-      return (
-        <fieldset disabled>
-          <div className="relative">
-            <DraftListButton existDraft={false} />
-          </div>
-        </fieldset>
-      );
-    }
-  };
-
-  const renderPopularPost = () => {
-    if (popular_post_res.post_data && popular_post_res.post_data.length > 0) {
-      return (
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
-          {popular_post_res.post_data.map((data: ResponsePostDataPlus) => {
-            return (
-              <Link key={data.id} href={`/post/${data.id}`}>
-                <PopularPostCard
-                  post_data={data}
-                  nickname={data.user.nickname}
-                  like_count={data.like[0].count}
-                />
-              </Link>
-            );
-          })}
-        </div>
-      );
-    } else {
-      return (
-        <div className="w-full min-h-140 flex items-center justify-center text-muted-foreground">
-          발행 된 게시글이 존재하지 않습니다.
-        </div>
-      );
-    }
-  };
-
-  const renderRecentPost = () => {
-    if (recent_post_res.post_data && recent_post_res.post_data.length > 0) {
-      return (
-        <div className="w-full grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
-          {recent_post_res.post_data.map((data: ResponsePostDataPlus) => {
-            return (
-              <Link key={data.id} href={`/post/${data.id}`}>
-                <NewPostCard
-                  post_data={data}
-                  nickname={data.user.nickname}
-                  comment_count={data.comment[0].count}
-                  like_count={data.like[0].count}
-                />
-              </Link>
-            );
-          })}
-        </div>
-      );
-    } else {
-      return (
-        <div className="w-full min-h-140 flex items-center justify-center text-muted-foreground">
-          발행 된 게시글이 존재하지 않습니다.
-        </div>
-      );
-    }
-  };
+  const draft_promise = handleDraftList();
+  const new_post_promise = handleRecentPostList();
+  const popular_post_promise = fetchPopularPost();
 
   return (
     <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
       <div className="flex gap-2 fixed right-1/2 bottom-10 translate-x-1/2 z-5 items-center">
         <NewPostButton onClickNewPost={onClickNewPost} />
-        {renderDraftDialog()}
+        <DraftList draft_promise={draft_promise} />
       </div>
       <Sidebar />
       <section className="flex-1 flex flex-col gap-12">
@@ -114,7 +39,11 @@ export default async function Home() {
             <p className="md:text-base text-muted-foreground">
               블로그 주인장의 인기있는 글을 모아봤어요.
             </p>
-            {renderPopularPost()}
+            <Suspense
+              fallback={<PostListSkeleton content="인기 게시글 로딩중..." />}
+            >
+              <PopularPostList popular_post_promise={popular_post_promise} />
+            </Suspense>
           </div>
         </div>
         <div className="w-full flex flex-col gap-6">
@@ -128,7 +57,11 @@ export default async function Home() {
             <p className="md:text-base text-muted-foreground">
               블로그 주인장이 새로운 글을 썼네요? 한 번 봐볼까요.
             </p>
-            {renderRecentPost()}
+            <Suspense
+              fallback={<PostListSkeleton content="최신 게시글 로딩중..." />}
+            >
+              <NewPostList new_post_promise={new_post_promise} />
+            </Suspense>
           </div>
         </div>
       </section>
